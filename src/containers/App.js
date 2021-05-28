f4adb184f31cbae944d1df0dc78b9aa2b8aebbd1
Vincent <mcken.vince@gmail.com>
2021-05-28 15:09:40 -0600

    upload robofriends app

---
+++ src/containers/App.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import CardList from '../components/CardList';
+import SearchBox from '../components/SearchBox';
+import Scroll from '../components/Scroll';
+import { robots } from '../robots'; // import robots from 'robots.js'
+import './app.css';
+
+class App extends React.Component {
+	constructor() {
+		super();
+		this.state = {
+			robots: [],
+			searchfield: ''
+		}
+	}
+
+	componentDidMount() {
+		// fetch('https://jsonplaceholder.typicode/com/users')
+		// .then(response => response.json())
+		// .then(users => {
+			this.setState({ robots: robots}) //swap to robots: users for json users file
+		// })
+	}
+
+	onSearchChange = (event) => {
+		this.setState({ searchfield: event.target.value });
+	}
+
+	render() {
+		const { robots, searchfield } = this.state;
+		const filteredRobots = robots.filter(robot => {
+			return robot.name.toLowerCase().includes(searchfield.toLowerCase());
+		})
+			return !robots.length ? // if no robots:
+			<h1>It looks like you have no robofriends... :(</h1> :
+			(
+				<div className='tc'>
+					<h1>Robofriends</h1>
+					<SearchBox searchChange={this.onSearchChange}/>
+					<Scroll>
+						<CardList robots={filteredRobots}/>
+					</Scroll>
+				</div>
+			);
+	}
+}
+
+export default App;
---
+++ src/robots.js
@@ -0,0 +1,122 @@
+export const robots = [
+{
+	id: 1,
+	name: 'Joey Schmoh',
+	username: 'JoMoLo',
+	email: 'joey.scmoh@yo.co'
+},
+{
+	id: 2,
+	name: 'Jeanette LaVita',
+	username: 'Jettevee',
+	email: 'jean@mean.lean'
+},
+{
+	id: 3,
+	name: 'Monica Pertie',
+	username: 'MonP',
+	email: 'pertiness@me.hee'
+},
+{
+	id: 4,
+	name: 'Trace Morris',
+	username: 'tractorMan',
+	email: 'tractorsrcool@tractors.com'
+},
+{
+	id: 5,
+	name: 'Carlton Berkis',
+	username: 'CBK',
+	email: 'carltonne.berkis@merkis.shmerkis'
+},
+{
+	id: 6,
+	name: 'Jiminy Yakutski',
+	username: 'Yatushi',
+	email: 'yakuji@mi.cee'
+},
+{
+	id: 7,
+	name: 'Fabriccio Noncielli',
+	username: 'FabFabrii',
+	email: 'fabriccio.latzio.noncielli@email.long'
+},
+{
+	id: 8,
+	name: 'Nance Poris',
+	username: 'NPM',
+	email: 'poris_n@nance.vance'
+},
+{
+	id: 9,
+	name: 'Wiggles Mee',
+	username: 'Wigwig',
+	email: 'meeeeeeeee@wiggles.jiggles'
+},
+{
+	id: 10,
+	name: 'Forrest Minarda',
+	username: 'Plantio',
+	email: 'iliketogrow@youknow.co'
+},
+{
+	id: 11,
+	name: 'Christopher Peters',
+	username: 'CPP',
+	email: 'christopher.peters@example.co'
+},
+{
+	id: 12,
+	name: 'William Bishop',
+	username: 'B-Wop',
+	email: 'willy-bness@example.co'
+},
+{
+	id: 13,
+	name: 'Wilma Cole',
+	username: 'ColeWE',
+	email: 'wilma_contact@mail.yes'
+},
+{
+	id: 14,
+	name: 'Aubrey Ray',
+	username: 'Ajax',
+	email: 'aubreyrs@example.co'
+},
+{
+	id: 15,
+	name: 'Zack Berry',
+	username: 'Zackyy',
+	email: 'zberr@example.co'
+},
+{
+	id: 16,
+	name: 'Connor Lambert',
+	username: 'Lambo',
+	email: 'lambchops_conn@example.co'
+},
+{
+	id: 17,
+	name: 'Terra Hansen',
+	username: 'Hansent',
+	email: 'earthy.hamster@example.co'
+},
+{
+	id: 18,
+	name: 'Eleanor Russell',
+	username: 'Ellie52',
+	email: 'ellie_52@example.co'
+},
+{
+	id: 19,
+	name: 'Vera Hawkins',
+	username: 'Vera09',
+	email: 'veering_sideways@example.co'
+},
+{
+	id: 20,
+	name: 'Thomas Woods',
+	username: 'WoodyWigglesworth',
+	email: 'woody_tom_@thebomb.co'
+}
+]
